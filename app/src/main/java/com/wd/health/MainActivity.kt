478b921e7ccc4586ee18430464a3cbee86ec7920
YCR <910447514@qq.com>
2020-05-20 21:33:59 +0800

    sp工具类修改

--- app/src/main/java/com/wd/health/MainActivity.kt
+++ app/src/main/java/com/wd/health/MainActivity.kt
@@ -2,16 +2,19 @@ package com.wd.health
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.wd.health.utils.SpUtil
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //
         //
-       var  keyname=SpUtil.DelegatesExt.spUtil("keyname","")
+
+
 
     }
 }
--- app/src/main/java/com/wd/health/utils/SpUtil.kt
+++ app/src/main/java/com/wd/health/utils/SpUtil.kt
@@ -11,40 +11,42 @@ import kotlin.reflect.KProperty
  * @date :2020/5/18 20:13
  * @classname :sendPrefens工具类
  */
-class SpUtil<T>(val name: String,val default:T) {
-    val prefs: SharedPreferences by lazy {
+class SpUtil {
+    val sharedPrefanaces: SharedPreferences by lazy {
         App.instance().getSharedPreferences("SP", Context.MODE_PRIVATE) }
+    val edit=sharedPrefanaces.edit();
 
-    operator fun getValue(
-        thisRef: Any?, property: KProperty<*>
-    ): T = getSharedPreferences(name, default)
-
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) = putSharedPreferences(name, value)
-
-    private fun putSharedPreferences(name: String, value: T) = with(prefs.edit()) {
-        when(value) {
-            is Int -> putInt(name, value)
-            is Float -> putFloat(name, value)
-            is Long -> putLong(name, value)
-            is Boolean -> putBoolean(name, value)
-            is String -> putString(name, value)
-            else -> throw IllegalArgumentException("SharedPreference can't be save this type")
-        }.apply()
-    }
-
-    private fun getSharedPreferences(name:String, default: T): T = with(prefs) {
-        val res: Any = when(default) {
-            is Int -> getInt(name, default)
-            is Float -> getFloat(name, default)
-            is Long -> getLong(name, default)
-            is Boolean -> getBoolean(name, default)
-            is String -> getString(name, default).toString()
-            else -> throw IllegalArgumentException("SharedPreference can't be get this type")
+    companion object{
+        private var instance:SpUtil? = null
+            get() {
+                if(field == null){
+                    field = SpUtil()
+                }
+                return field
+            }
+        fun getSpUtil():SpUtil{
+            return instance!!
         }
-        return res as T
     }
-    object DelegatesExt {
-        fun <T> spUtil(name: String, default: T) = SpUtil(name, default)
+
+    //存储
+    fun putString(key:String,value:String){
+        edit.putString(key,value)
+        edit.commit()
+    }
+    fun putInt(key:String,value:Int){
+        edit.putInt(key,value)
+        edit.commit()
+    }
+
+    //取出
+    fun getString(key:String): String? {
+        val str = sharedPrefanaces.getString(key, null)
+        return str
+    }
+    fun getInt(key:String): Int? {
+        val int = sharedPrefanaces.getInt(key, -1)
+        return int
     }
 }
 
