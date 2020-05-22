b250e56dd4260c3be4aa978cc41343ec17747135
quzijun <756547637@qq.com>
2020-05-22 16:41:50 +0800

    05-22:登录注册布局

---
+++ app/src/main/java/com/wd/health/ui/activity/RegActivity.java
@@ -0,0 +1,11 @@
+package com.wd.health.ui.activity;
+
+/**
+ * ClassName: Health
+ *
+ * @author 作者 : Quzijun
+ * @version 创建时间：2020/5/22 16:41
+ * @Description: 用途：完成特定功能
+ */
+public class RegActivity {
+}
